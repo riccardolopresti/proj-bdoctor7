4fd0716c1d5528e5d6d9565f1bd674fe95f4ec73
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-02-14 18:23:03 +0100

    [add] seeder of User, Doctors

--- database/seeders/DoctorSeeder.php
+++ database/seeders/DoctorSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Doctor;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class DoctorSeeder extends Seeder
 {
@@ -12,8 +16,19 @@ class DoctorSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $users= User::all();
+
+        foreach($users as $user){
+            $new_doctor = new Doctor();
+            $new_doctor->surname = $faker->lastName();
+            $new_doctor->slug = Str::slug($user->name . '-' . $new_doctor->surname);
+            $new_doctor->address = $faker->streetAddress();
+            $new_doctor->image = 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png';
+            $new_doctor->phone = $faker->phoneNumber();
+            $new_doctor->health_care = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $new_doctor->save();
+        }
     }
 }
