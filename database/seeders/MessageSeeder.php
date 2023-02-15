0b740beeafe190f4d9b9384f57f72ee679c0860e
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-02-15 10:18:13 +0100

    [add] seeder of message, offer

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Doctor;
+use App\Models\Message;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
 
 class MessageSeeder extends Seeder
 {
@@ -12,8 +16,20 @@ class MessageSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+
+
+        for ($i=0; $i < 20; $i++) {
+
+
+            $new_message = new Message();
+            $new_message->name = $faker->name();
+            $new_message->text = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $new_message->email = $faker->email();
+            $doctor_id = Doctor::inRandomOrder()->first()->id;
+            $new_message->doctor_id = $doctor_id;
+            $new_message->save();
+        }
     }
 }
--- database/seeders/OfferSeeder.php
+++ database/seeders/OfferSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Offer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class OfferSeeder extends Seeder
 {
@@ -14,6 +16,20 @@ class OfferSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('offers')->insert([
+            "offer_type" => "basic",
+            "price" => 2.99,
+            "duration" => 24
+        ]);
+        DB::table('offers')->insert([
+            "offer_type" => "standard",
+            "price" => 5.99,
+            "duration" => 72
+        ]);
+        DB::table('offers')->insert([
+            "offer_type" => "premium",
+            "price" => 9.99,
+            "duration" => 144
+        ]);
     }
 }
