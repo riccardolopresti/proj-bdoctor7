40518d4183fe657839e9a5bcf243028a8fcaccf3
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-02-15 15:25:50 +0100

    [add] aside header footer

---
+++ resources/views/admin/partials/aside.blade.php
@@ -0,0 +1,19 @@
+<aside>
+    <ul>
+        <li>
+            <a href="#">sdadfsada</a>
+        </li>
+        <li>
+            <a href="#">dsadsa</a>
+        </li>
+        <li>
+            <a href="#">dasdas</a>
+        </li>
+        <li>
+            <a href="#">dsada</a>
+        </li>
+        <li>
+            <a href="#">dasdas</a>
+        </li>
+    </ul>
+</aside>
