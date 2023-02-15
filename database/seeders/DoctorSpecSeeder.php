5c0b70f39d532e99591f83328323bf2508a0572d
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-02-15 11:16:12 +0100

    [add] seeder of doctorSpec

---
+++ database/seeders/DoctorSpecSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Doctor;
+use App\Models\Spec;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DoctorSpecSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $doctors = Doctor::all();
+
+        foreach($doctors as $doctor){
+            $random_numb = rand(1,3);
+            for ($i=0; $i < $random_numb ; $i++) {
+                $spec_id = Spec::inRandomOrder()->first();
+
+                if(!$doctor->specs->contains($spec_id)){
+                    $doctor->specs()->attach($spec_id);
+                }
+            }
+        }
+
+        //dump($doctor->specs());
+
+    }
+}
