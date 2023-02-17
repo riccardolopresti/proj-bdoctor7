1d20cbcef25f707f8239648048ed279f9fac1f5f
andreaa21 <andrea.zollini@proton.me>
2023-02-17 17:27:01 +0100

    [add] registration with all fields of doctors

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Doctor;
+use App\Models\Spec;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -12,6 +14,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
+use Illuminate\Support\Str;
 
 class RegisteredUserController extends Controller
 {
@@ -20,7 +23,8 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        $specs = Spec::all();
+        return view('auth.register', compact('specs'));
     }
 
     /**
@@ -32,7 +36,9 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'surname' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -41,6 +47,21 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $doctors = Doctor::create([
+            'user_id' => $user->id,
+            'surname' => $request->surname,
+            'slug' => Str::slug($request->name . '-' . $request->surname),
+            'address' => $request->address
+        ]);
+
+        // $form_data['slug'] = Str::slug($form_data['name']);
+        // Doctor::generateSlug(Auth::user()->name, $form_data['surname'])
+        // $specs = Spec::create([
+
+        // ])
+
+        // dd($request);
 
         event(new Registered($user));
 
