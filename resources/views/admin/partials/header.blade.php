1d0e67cad56487ae7023d7aa813c0b1e9e7914fe
saracassandra7 <saracassandra7@gmail.com>
2023-02-18 17:46:04 +0100

    [fix] header

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -2,32 +2,21 @@
     <nav class="navbar navbar-expand-md">
         <div class="container-fluid">
             <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                <div class="logo_doc mb-3">
+                <div class="logo_doc">
                     <img src="/logo-doc-transp.png" alt="logo">
                 </div>
                 {{-- config('app.name', 'Laravel') --}}
             </a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{url('/') }}"><i class="fa-solid fa-house fs-5 mb-2 ps-2"></i></a>
-                    </li>
-                    {{-- <li>
-                        <form class="search-form mt-2 ms-5">
-                            <button class="search-btn" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                            <input type="text" placeholder="Cerca...">
-                        </form>
-                    </li> --}}
-                </ul>
+            <div class="d-flex">
+                <!-- Left Side Of Navbar-->
+                {{-- <form class="search-form mt-2 ms-5">
+                    <button class="search-btn" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    <input type="text" placeholder="Cerca...">
+                </form> --}}
 
                 <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
+                <ul class="d-flex header-menu">
                     <!-- Authentication Links -->
                     @guest
                     <li class="nav-item">
@@ -40,6 +29,9 @@
                     @endif
 
                     @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{url('/') }}"><i class="fa-solid fa-house fs-5"></i></a>
+                    </li>
                     <li class="nav-item me-2 mb-2">
                         <a class="nav-link" href="{{ url('admin') }}">{{__('Dashboard')}}</a>
                     </li>
