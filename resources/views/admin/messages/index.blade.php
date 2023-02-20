e06e9e6eb5afaaff845b9447a86c92e2ae0e2c86
RiccardoLoPresti <riccardolopresti35@gmail.com>
2023-02-20 17:08:29 +0100

    [add] link to tr

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col p-0">
+            <div class="col custom-messages-col">
 
                 <div class="title py-3">
                     <h1>Messaggi</h1>
@@ -29,7 +29,7 @@
                             <div class="container m-0 p-0">
                                 <div class="wrap-table100">
                                     <div class="table100">
-                                            @foreach ($doctors as $doctor )
+                                        @foreach ($doctors as $doctor )
                                             <table class="my-5">
                                                 <thead>
                                                     <tr class="table100-head">
@@ -44,102 +44,39 @@
                                                 <tbody>
                                                     @forelse ($doctor->messages as $message)
                                                     <tr>
-                                                        <td>{{ $doctor->surname }}</td>
-                                                        <td>{{ $message->name }}</td>
-                                                        <td>{{ $message->object }}</td>
-                                                        <td>{{ $message->email }}</td>
-                                                        <td>{{ $message->text }}</td>
-                                                        <td>
-                                                            <div class="delete-form">
-                                                                @include('admin.messages.partials.delete-form')
-                                                                </form>
-                                                            </div>
-                                                        </td>
+                                                        <a href="https://logoipsum.com/">
+                                                            <td>
+                                                                <a href="http://www.example.com/">
+                                                                    {{ $loop->iteration }}
+                                                                </a>
+                                                            </td>
+                                                            <td>{{ $message->name }}</td>
+                                                            <td>{{ $message->object }}</td>
+                                                            <td>{{ $message->email }}</td>
+                                                            <td>{{ $message->text }}</td>
+                                                            <td>
+                                                                <div class="delete-form">
+                                                                    @include('admin.messages.partials.delete-form')
+                                                                    </form>
+                                                                </div>
+                                                            </td>
+                                                        </a>
                                                     </tr>
-                                                @empty
-                                                    <tr>
-                                                        <td colspan="4">Nessun messaggio...</td>
-                                                    </tr>
-                                                @endforelse
-                                            </table>
-                                            </div>
-                                            @endforeach
-                                        </div>
 
+                                                    @empty
+                                                        <tr>
+                                                            <td colspan="4">Nessun messaggio...</td>
+                                                        </tr>
+                                                    @endforelse
+                                                </tbody>
+                                            </table>
+                                        @endforeach
+                                    </div>
                                 </div>
                             </div>
+                        </div>
                     </div>
-
-                    {{-- @foreach ($doctors as $doctor )
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th style="width: 200px" scope="col">
-                                        Dott.{{ $doctor->surname }}
-                                    </th>
-                                    <th scope="col">Nome Utente</th>
-                                    <th scope="col">Oggetto</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col">Messaggio</th>
-                                    <th scope="col">Azioni</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @forelse ($doctor->messages as $message)
-                                    <tr>
-                                        <td>{{$loop->iteration}}</td>
-                                        <td>{{ $message->name }}</td>
-                                        <td>{{ $message->object }}</td>
-                                        <td>{{ $message->email }}</td>
-                                        <td>{{ $message->text }}</td>
-                                        <td>
-                                            <div class="delete-form">
-                                                @include('admin.messages.partials.delete-form')
-                                                </form>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="4">Nessun messaggio...</td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-
-                        @endforeach
-                    {{ $doctors->links() }} --}}
                 @else
-                    {{-- <div class="wrapper border bbord rounded-4 my-3">
-
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th style="width: 200px" scope="col" class="text-capitalize">
-                                        Dott. {{ $user_logged->surname }}
-                                    </th>
-                                    <th scope="col">Nome Utente</th>
-                                    <th scope="col">Oggetto</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col">Messaggio</th>
-                            </thead>
-                            <tbody>
-                                @forelse ( $user_logged->messages as $message)
-                                    <tr>
-                                        <td>{{$loop->iteration}}</td>
-                                        <td>{{ $message->name }}</td>
-                                        <td>{{ $message->object }}</td>
-                                        <td>{{ $message->email }}</td>
-                                        <td>{{ $message->text }}</td>
-                                    </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="4">Nessun messaggio...</td>
-                                    </tr>
-                                @endforelse
-                            </tbody>
-                        </table>
-                    </div> --}}
 
                     <div class="special-table">
                         <div class="limiter m-0">
@@ -178,6 +115,24 @@
                         </div>
                     </div>
                 @endif
+            @if (Auth::user()->is_admin)
+                {{ $doctors->links() }}
+            @endif
+
             </div>
         </div>
+
+        <style>
+            .col.custom-messages-col{
+                padding-bottom: 200px
+            }
+        </style>
+
+        <script>
+            $('tr').click( function() {
+                window.location = $(this).find('a').attr('href');
+            }).hover( function() {
+                $(this).toggleClass('hover');
+            });
+        </script>
 @endsection
