8291f80ee38625f4ecde3b9922a5adad3ec16e52
andreaa21 <andrea.zollini@proton.me>
2023-02-21 17:12:20 +0100

    [add] name function

--- resources/js/pages/Search.vue
+++ resources/js/pages/Search.vue
@@ -32,6 +32,14 @@ export default {
                     console.log(result.data.filteredDoctors[0].doctors);
                 });
         },
+        getName(str) {
+            const array = str.split("-");
+            for (let i = 0; i < array.length; i++) {
+                array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1);
+            }
+            const name = array.join(" ");
+            return name;
+        },
     },
     mounted() {
         this.getDoctors();
@@ -56,7 +64,7 @@ export default {
             :key="doctor.id"
             class="doctor-card"
         >
-            <p>{{ doctor.surname }}</p>
+            <p>{{ getName(doctor.slug) }}</p>
             <p>{{ doctor.phone }}</p>
             <p>{{ doctor.address }}</p>
             <!-- <ul>
