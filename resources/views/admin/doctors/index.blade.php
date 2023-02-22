3c26b722349025f805068ac7c10c869512bfc683
andreaa21 <andrea.zollini@proton.me>
2023-02-22 19:22:03 +0100

    [fix] profile page

--- resources/views/admin/doctors/index.blade.php
+++ resources/views/admin/doctors/index.blade.php
@@ -5,34 +5,34 @@
     <div class="container-fluid">
         <div class="main-wrapper-doctors row">
             <div class="col-12 d-flex justify-content-end buttons mb-3">
-                <a href="{{route('admin.doctors.edit', $doctor)}}" class="bn632-hover bn26 me-2 edit-profile"><i class="fa-solid fa-pen-to-square"></i>&nbsp;Modifica profilo</a>
-                <a href="{{route('admin.messages.index')}}" class="bn632-hover bn26 me-2 d-none d-md-flex"><i class="fa-solid fa-envelope"></i>&nbsp;Messages</a>
-                <a href="{{route('admin.offers.index')}}" class="bn632-hover bn26 me-2 d-none d-md-flex"><i class="fa-solid fa-award"></i>&nbsp;Promo</a>
-                <button type="button" class="bn632-hover bn26 d-none d-md-inline-block" disabled><i class="fa-solid fa-file-invoice"></i>&nbsp;CV</button>
+                <a href="{{ route('admin.doctors.edit', $doctor) }}" class="bn632-hover bn26 me-2 edit-profile"><i
+                        class="fa-solid fa-pen-to-square"></i>&nbsp;Modifica profilo</a>
+                <a href="{{ route('admin.messages.index') }}" class="bn632-hover bn26 me-2 d-none d-md-flex"><i
+                        class="fa-solid fa-envelope"></i>&nbsp;Messages</a>
+                <a href="{{ route('admin.offers.index') }}" class="bn632-hover bn26 me-2 d-none d-md-flex"><i
+                        class="fa-solid fa-award"></i>&nbsp;Promo</a>
+                <button type="button" class="bn632-hover bn26 d-none d-md-inline-block" disabled><i
+                        class="fa-solid fa-file-invoice"></i>&nbsp;CV</button>
             </div>
             <h1 class="blue text-center mb-4">Il tuo profilo</h1>
             <div class="col-lg-7 col-12 left row">
 
                 <div class="col-sm-5 col-12 profile-photo p-0">
-                    @if (str_contains($doctor->image,'http'))
-                    <img src="{{$doctor->image}}"
-                    class="card-img-top"
-                    alt="{{$doctor->slug}}"
-                    title="Anteprima dottore">
+                    @if (str_contains($doctor->image, 'http'))
+                        <img src="{{ $doctor->image }}" class="card-img-top" alt="{{ $doctor->slug }}"
+                            title="Anteprima dottore">
                     @else
-                    <img src="{{ asset('storage/'.$doctor->image)}}"
-                    class="card-img-top"
-                    alt="{{$doctor->image_original_name}}"
-                    title="Anteprima dottore">
+                        <img src="{{ asset('storage/' . $doctor->image) }}" class="card-img-top"
+                            alt="{{ $doctor->image_original_name }}" title="Anteprima dottore">
                     @endif
                 </div>
                 <div class="col-sm-7 details col-12">
-                    <h4 class="text blue mt-2">{{$user->name}} {{$doctor->surname}}</h4>
-                  <h6 class="profile-section mt-3">
-                      Specializzazioni: </h6>
-                      <ul class="list-unstyled list-inline mb-3">
+                    <h4 class="text blue mt-2">{{ $user->name }} {{ $doctor->surname }}</h4>
+                    <h6 class="profile-section mt-3">
+                        Specializzazioni: </h6>
+                    <ul class="list-unstyled list-inline mb-3">
                         @foreach ($doctor->specs as $spec)
-                            <li class="my-2 d-inline me-2 mt-4"><span class="badge spec-badge">{{$spec->type}}</span>
+                            <li class="my-2 d-inline me-2 mt-4"><span class="badge spec-badge">{{ $spec->type }}</span>
                             </li>
                         @endforeach
                     </ul>
@@ -40,15 +40,15 @@
                 </div>
                 <div class="contacts col-12">
                     <p class="mx-0 mt-3">
-                            <i class="fa-solid blue fa-location-dot"></i></i>&nbsp; {{$doctor->address}}
+                        <i class="fa-solid blue fa-location-dot"></i></i>&nbsp; {{ $doctor->address }}
                     </p>
 
                     <p class="mt-2"><i class="fa-solid blue fa-phone"></i>&nbsp;
-                            @if ($doctor->phone)
-                            {{$doctor->phone}}
-                            @else
+                        @if ($doctor->phone)
+                            {{ $doctor->phone }}
+                        @else
                             <span class="grey">Nessun telefono inserito</span>
-                            @endif
+                        @endif
                     </p>
 
                 </div>
@@ -58,9 +58,9 @@
                         <h6 class="light-blue"><i class="fa-solid fa-notes-medical"></i> I tuoi servizi</h6>
 
                         @if ($doctor->health_care)
-                        {!! $doctor->health_care !!}
+                            {!! $doctor->health_care !!}
                         @else
-                        <span class="grey">Nessun servizio inserito</span>
+                            <span class="grey">Nessun servizio inserito</span>
                         @endif
                     </div>
 
@@ -70,20 +70,20 @@
             <div class="col-lg-5 col-12 right d-flex flex-column justify-content-end align-items-end row">
                 <div class="sections-blue mt-4 col-12">
                     <div class="section-blue ratings-section">
-                        <a href="{{route('admin.ratings.index')}}" class="text-decoration-none text-white">
+                        <a href="{{ route('admin.ratings.index') }}" class="text-decoration-none text-white">
                             <h6>Le tue valutazioni</h6>
 
                             <div class="bg-container">
                                 @if ($doc_ratings)
                                     <span class="ratings-container"></span>
                                 @else
-                                <span class="grey">
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>
-                                    <i class="fa-solid fa-star"></i>&nbsp;
-                                    Non hai ancora ricevuto valutazioni</span>
+                                    <span class="grey">
+                                        <i class="fa-solid fa-star"></i>
+                                        <i class="fa-solid fa-star"></i>
+                                        <i class="fa-solid fa-star"></i>
+                                        <i class="fa-solid fa-star"></i>
+                                        <i class="fa-solid fa-star"></i>&nbsp;
+                                        Non hai ancora ricevuto valutazioni</span>
                                 @endif
                             </div>
                         </a>
@@ -107,19 +107,19 @@
 
                                             <ul class="slides-container" id="slides-container">
                                                 @foreach ($doc_reviews as $doc_review)
-                                              <li class="slide">
-                                                    <div class="name-and-time d-flex justify-content-between mb-3 flex-wrap">
-                                                            <h6>{{$doc_review->name}}</h6>
+                                                    <li class="slide">
+                                                        <div
+                                                            class="name-and-time d-flex justify-content-between mb-3 flex-wrap">
+                                                            <h6>{{ $doc_review->name }}</h6>
                                                             <div class="time"></div>
-                                                    </div>
-                                                    <p class="review-content">{{$doc_review->text}}</p>
-                                              </li>
-                                                 @endforeach
+                                                        </div>
+                                                        <p class="review-content">{{ $doc_review->text }}</p>
+                                                    </li>
+                                                @endforeach
                                             </ul>
                                         </section>
-
                                     @else
-                                    <span class="grey">Non hai ancora ricevuto recensioni</span>
+                                        <span class="grey">Non hai ancora ricevuto recensioni</span>
                                     @endif
                                 </span>
                             </div>
@@ -139,30 +139,29 @@
 @push('body-scripts')
     @once
         <script>
-
-            function starsRating(number){
-                let newRating = (Math.ceil(number*2)/2);
+            function starsRating(number) {
+                let newRating = (Math.ceil(number * 2) / 2);
                 let stars = [];
-                const diff=5-newRating;
+                const diff = 5 - newRating;
                 console.log(newRating, diff);
-                    for(let i=newRating; i>=1; i--){
-                        stars.push(`<i class="fa-solid fa-star" style="color:gold;"></i>`);
+                for (let i = newRating; i >= 1; i--) {
+                    stars.push(`<i class="fa-solid fa-star" style="color:gold;"></i>`);
 
                 };
-                if(diff % 1){
+                if (diff % 1) {
                     stars.push(`<i class="fa-solid fa-star-half-stroke" style="color:gold;"></i>`);
                 }
-                for(let j = (5 - newRating); j >= 1; j--){
-                stars.push(`<i class="fa-regular fa-star" style="color:gold"></i>`);
+                for (let j = (5 - newRating); j >= 1; j--) {
+                    stars.push(`<i class="fa-regular fa-star" style="color:gold"></i>`);
                 }
                 return stars.join('');
 
             }
 
             const data = "<?php echo $doc_ratings; ?>";
-            const reviewContainer=document.querySelector('.ratings-container')
-                $(document).ready(function() {
-                    reviewContainer.innerHTML=starsRating(data);
+            const reviewContainer = document.querySelector('.ratings-container')
+            $(document).ready(function() {
+                reviewContainer.innerHTML = starsRating(data);
             });
 
             const slidesContainer = document.getElementById("slides-container");
@@ -170,30 +169,25 @@
             const prevButton = document.getElementById("slide-arrow-prev");
             const nextButton = document.getElementById("slide-arrow-next");
             nextButton.addEventListener("click", () => {
-            const slideWidth = slide.clientWidth;
-            slidesContainer.scrollLeft += slideWidth;
+                const slideWidth = slide.clientWidth;
+                slidesContainer.scrollLeft += slideWidth;
             });
             prevButton.addEventListener("click", () => {
-            const slideWidth = slide.clientWidth;
-            slidesContainer.scrollLeft -= slideWidth;
+                const slideWidth = slide.clientWidth;
+                slidesContainer.scrollLeft -= slideWidth;
             });
 
-            function changeDateFormat(date){
-                let new_date=date.substring(0,10).split("-").reverse().slice().join("/");
+            function changeDateFormat(date) {
+                let new_date = date.substring(0, 10).split("-").reverse().slice().join("/");
                 return new_date
             }
 
             const docReviews = <?php echo json_encode($doc_reviews); ?>
-            // let date = "<?php echo $doc_review->created_at; ?>";
 
-            for(let i=0; i<docReviews.length; i++){
-                let putDates=document.querySelectorAll('.time')
-                putDates[i].innerHTML=changeDateFormat(docReviews[i]['created_at'])
+            for (let i = 0; i < docReviews.length; i++) {
+                let putDates = document.querySelectorAll('.time')
+                putDates[i].innerHTML = changeDateFormat(docReviews[i]['created_at'])
             }
-
-
-
-
         </script>
     @endonce
 @endpush
