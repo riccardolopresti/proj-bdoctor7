c1079c142d4f4cb5c6e5d243dd207cdd07d57098
saracassandra7 <saracassandra7@gmail.com>
2023-02-22 17:47:46 +0100

    [add] sponsored doctors section

---
+++ resources/js/pages/SponsorSection.vue
@@ -0,0 +1,32 @@
+<script>
+import DocCard from '../components/DocCard.vue';
+import docs from '../assets/data/sponsored-docs'
+
+export default {
+    name: 'SponsorSection',
+
+    components:{
+        DocCard
+    },
+
+    data(){
+    return{
+      docs
+    }
+  }
+}
+</script>
+
+<template>
+<section>
+       <h1 class="fw-bold text-center mt-4 text-primary">Medici in evidenza</h1>
+    <div class="container-fluid">
+        <DocCard v-for="(doc, index) in docs" :key="index" :docName="doc.name" :docImg="doc.image" :docSpec="doc.specialization" :docMail="doc.email" :docCare="doc.description" />
+    </div>
+</section>
+
+</template>
+
+
+<style scoped>
+</style>
