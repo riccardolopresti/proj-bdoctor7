1a5075db0ceb5379ee22d1088d4fdb1a4ad3ed2a
saracassandra7 <saracassandra7@gmail.com>
2023-02-24 18:16:02 +0100

    [start] footer

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -2,7 +2,8 @@
 
 import Header from "../partials/Header.vue";
 import Jumbotron from "../partials/Jumbotron.vue";
-import SponsorSection from './SponsorSection.vue'
+import SponsorSection from './SponsorSection.vue';
+import Footer from "../partials/Footer.vue";
 
 import axios from "axios";
 
@@ -14,7 +15,8 @@ export default {
     components: {
         Header,
         Jumbotron,
-        SponsorSection
+        SponsorSection,
+        Footer
     },
     data() {
         return {
@@ -43,7 +45,8 @@ export default {
 
     <Header />
     <Jumbotron :specs="specs" />
-    <SponsorSection/>>
+    <SponsorSection/>
+    <Footer/>
 
 </template>
 
