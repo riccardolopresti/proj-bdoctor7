d1ff64d9632a3155060d97a19c94ba920f33b179
Riccardo Lo Presti <109863052+riccardolopresti@users.noreply.github.com>
2023-02-26 11:23:26 +0100

    [add] grphics need to fix queries to bd

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('admin.home');
+
+        $user_logged =  Auth()->user()->doctors->ratings;
+
+        return view('admin.home', compact('user_logged'));
     }
 }
