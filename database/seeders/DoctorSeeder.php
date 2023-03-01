f6453fbd01b4db62de6fa1174b54ddbe543bcb57
Riccardo Lo Presti <109863052+riccardolopresti@users.noreply.github.com>
2023-03-01 12:29:00 +0100

    [add] seeder review and health care services

--- database/seeders/DoctorSeeder.php
+++ database/seeders/DoctorSeeder.php
@@ -20,6 +20,58 @@ class DoctorSeeder extends Seeder
     {
         $users = User::all();
 
+        $health_care_services = [
+            "Analisi del sangue",
+            "Test di gravidanza",
+            "Vaccinazioni",
+            "Test delle allergie",
+            "Visite di routine dal medico di base",
+            "Controllo della glicemia",
+            "Test dell'HIV",
+            "Radiografie",
+            "Ecografia",
+            "Colonscopia",
+            "Endoscopia",
+            "Esami del sangue per la diagnosi del cancro",
+            "Elettrocardiogramma (ECG)",
+            "Esami del sangue per la diagnosi delle malattie cardiache",
+            "Esame degli occhi",
+            "Esame dell'udito",
+            "Esame della vista",
+            "Controllo del peso",
+            "Controllo della funzionalità renale",
+            "Test della funzionalità epatica",
+            "Esame del tratto urinario",
+            "Esame del sangue per la diagnosi delle malattie epatiche",
+            "Test della funzionalità tiroidea",
+            "Esame del sistema nervoso",
+            "Esame delle funzioni polmonari",
+            "Esame della funzione renale",
+            "Esame della funzione cardiaca",
+            "Esame delle funzioni digestive",
+            "Esame della funzione muscolare",
+            "Esame della funzione articolare",
+            "Esame della funzione respiratoria",
+            "Esame della funzione del sistema immunitario",
+            "Esame della funzione del sistema endocrino",
+            "Esame della funzione del sistema riproduttivo",
+            "Esame delle funzioni cognitive",
+            "Esame della funzione psicologica",
+            "Esame delle funzioni emotive",
+            "Esame delle funzioni sociali",
+            "Esame delle funzioni spirituali",
+            "Trattamento della depressione",
+            "Trattamento dell'ansia",
+            "Trattamento della schizofrenia",
+            "Trattamento del disturbo bipolare",
+            "Trattamento dei disturbi del sonno",
+            "Trattamento dell'asma",
+            "Trattamento del diabete",
+            "Trattamento dell'ipertensione",
+            "Trattamento delle malattie cardiache",
+            "Trattamento del cancro"
+        ];
+
         foreach ($users as $user) {
             $new_doctor = new Doctor();
             $new_doctor->user_id = $user->id;
@@ -28,7 +80,7 @@ class DoctorSeeder extends Seeder
             $new_doctor->address = $faker->streetAddress();
             $new_doctor->image = 'https://www.indrenetwork.com/sites/default/files/2019-11/silueta_6_0.png';
             $new_doctor->phone = $faker->phoneNumber();
-            $new_doctor->health_care = $faker->realText($maxNbChars = 200, $indexSize = 2);
+            $new_doctor->health_care = $health_care_services[array_rand(array_values($health_care_services))];
             $new_doctor->save();
         }
     }
