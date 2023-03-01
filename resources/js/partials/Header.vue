67b698787b1833370c8a34260c33040e60848cf0
andreaa21 <andrea.zollini@proton.me>
2023-03-01 12:01:39 +0100

    [fix] header

--- resources/js/partials/Header.vue
+++ resources/js/partials/Header.vue
@@ -1,68 +1,68 @@
 <script>
 export default {
-    name: 'Header',
+    name: "Header",
 
-    methods:{
-        toggleMenu(){
+    methods: {
+        toggleMenu() {
             const hamburger = document.querySelector(".hamburger");
             const navLinks = document.querySelector(".nav-links");
             const links = document.querySelectorAll(".nav-links li");
             //Links
             navLinks.classList.toggle("open");
-            links.forEach(link => {
-            link.classList.toggle("fade");
+            links.forEach((link) => {
+                link.classList.toggle("fade");
             });
 
             //Animation
             hamburger.classList.toggle("toggle");
-        }
-    }
-}
-
+        },
+    },
+};
 </script>
 
 <template>
     <header>
         <nav>
-        <div class="logo">
-            <router-link :to="{ name: 'home'}">
-                <img src="/logo-doc-transp.png" alt="Logo Doc">
-            </router-link>
-
-        </div>
-        <div @click="toggleMenu" class="hamburger">
-            <div class="bars1"></div>
-            <div class="bars2"></div>
-            <div class="bars3"></div>
-        </div>
-        <ul class="nav-links m-0">
-            <li><router-link :to="{ name: 'home'}">Home</router-link></li>
-            <li><a href="#about-us">chi siamo</a></li>
-            <li><a href="#spons-doc">Medici in evidenza</a></li>
-            <li><a href="/login" id="login-button">Sign in</a></li>
-        </ul>
+            <div class="logo">
+                <router-link :to="{ name: 'home' }">
+                    <img src="/logo-doc-transp.png" alt="Logo Doc" />
+                </router-link>
+            </div>
+            <div @click="toggleMenu" class="hamburger">
+                <div class="bars1"></div>
+                <div class="bars2"></div>
+                <div class="bars3"></div>
+            </div>
+            <ul class="nav-links">
+                <li><router-link :to="{ name: 'home' }">Home</router-link></li>
+                <li v-if="$route.path !== '/search'">
+                    <a href="#about-us">chi siamo</a>
+                </li>
+                <li v-if="$route.path !== '/search'">
+                    <a href="#spons-doc">Medici in evidenza</a>
+                </li>
+                <li><a href="/login" id="login-button">Accedi</a></li>
+            </ul>
         </nav>
     </header>
 </template>
 
-
 <style scoped>
-*{
-    color:#f2f5f7;
+* {
+    color: #f2f5f7;
     letter-spacing: 1px;
     font-weight: 300;
 }
 
-header{
+header {
     height: 96px;
 }
 
-body{
+body {
     overflow: hidden;
-
 }
 
-nav{
+nav {
     height: 6rem;
     width: 100vw;
     display: flex;
@@ -70,25 +70,23 @@ nav{
     justify-content: space-between;
     position: fixed;
     z-index: 10;
-    background: rgb(55,130,232);
-    box-shadow: 0 3px 20px rgba(0,0,0,0.3);
+    background: rgb(55, 130, 232);
+    box-shadow: 0 3px 20px rgba(0, 0, 0, 0.3);
 }
-
 
 /* Styling Logo*/
 
-.logo{
+.logo {
     width: 250px;
 }
 
-.logo img{
+.logo img {
     max-width: 100%;
-
 }
 
 /* Styling Navigation Links*/
 
-.nav-links{
+.nav-links {
     width: 80vw;
     display: flex;
     padding: 0 0.7vw;
@@ -97,52 +95,54 @@ nav{
     text-transform: uppercase;
     list-style: none;
     font-weight: 600;
+    margin: 0;
 }
 
-.nav-links li a{
+.nav-links li a {
     margin: 0 0.7vw;
     text-decoration: none;
     transition: all ease-in-out 350ms;
     padding: 10px;
 }
 
-.nav-links li a:hover, .nav-links li a.active{
-    color:#000;
+.nav-links li a:hover,
+.nav-links li a.active {
+    color: #000;
     background-color: #fff;
     padding: 10px;
     border-radius: 50px;
 }
 
-.nav-links li{
-    position:relative;
+.nav-links li {
+    position: relative;
 }
 
-.nav-links li a:hover::before{
+.nav-links li a:hover::before {
     width: 80%;
 }
 
 /*Buttons Styling*/
 
-#login-button{
+#login-button {
     padding: 0.6rem 0.8rem;
-    margin-left: 2vw;
-    font-size:1rem;
-    cursor:pointer;
+    /* margin-left: 2vw; */
+    font-size: 1rem;
+    cursor: pointer;
     background-color: transparent;
-    border:1.5px solid #f2f5f7;
+    border: 1.5px solid #f2f5f7;
     border-radius: 2em;
 }
 
-#login-button:hover{
-    color:#fff;
+#login-button:hover {
+    color: #fff;
     background-color: #dd5f24;
-    border:1.5px solid #dd5f24;
+    border: 1.5px solid #dd5f24;
     transition: all ease-in-out 350ms;
 }
 
 /*Navigation Icon Styling*/
 
-.hamburger div{
+.hamburger div {
     width: 30px;
     height: 3px;
     background: #f2f5f7;
@@ -150,20 +150,19 @@ nav{
     transition: all 0.3s ease;
 }
 
-.hamburger{
+.hamburger {
     display: none;
 }
 
-
 /*Responsive Adding Media Queries*/
 
-@media screen and (max-width: 800px){
-    nav{
+@media screen and (max-width: 800px) {
+    nav {
         position: fixed;
         z-index: 3;
     }
-    .hamburger{
-        display:block;
+    .hamburger {
+        display: block;
         position: absolute;
         cursor: pointer;
         right: 5%;
@@ -172,70 +171,72 @@ nav{
         z-index: 2;
         transition: all 0.7s ease;
     }
-    .nav-links{
-        background: rgb(55,130,232);
+    .nav-links {
+        background: rgb(55, 130, 232);
         position: fixed;
         bottom: 50%;
         opacity: 1;
         height: 50vh;
-
         width: 100%;
         flex-direction: column;
+        justify-content: start;
         clip-path: circle(50px at 90% -20%);
         -webkit-clip-path: circle(50px at 90% -10%);
         transition: all 1s ease-out;
         pointer-events: none;
     }
-    .nav-links.open{
+    .nav-links.open {
         clip-path: circle(1000px at 90% -10%);
         -webkit-clip-path: circle(1000px at 90% -10%);
         pointer-events: all;
+        padding-top: 80px;
     }
-    .nav-links li{
+    .nav-links li {
         opacity: 0;
+        margin: 20px 0;
     }
-    .nav-links li:nth-child(1){
+    .nav-links li:nth-child(1) {
         transition: all 0.5s ease 0.2s;
+        margin: 40px 0 10px 0;
     }
-    .nav-links li:nth-child(2){
+    .nav-links li:nth-child(2) {
         transition: all 0.5s ease 0.4s;
     }
-    .nav-links li:nth-child(3){
+    .nav-links li:nth-child(3) {
         transition: all 0.5s ease 0.6s;
     }
-    .nav-links li:nth-child(4){
+    .nav-links li:nth-child(4) {
         transition: all 0.5s ease 0.7s;
     }
-    .nav-links li:nth-child(5){
+    .nav-links li:nth-child(5) {
         transition: all 0.5s ease 0.8s;
     }
-    .nav-links li:nth-child(6){
+    .nav-links li:nth-child(6) {
         transition: all 0.5s ease 0.9s;
         margin: 0;
     }
-    .nav-links li:nth-child(7){
+    .nav-links li:nth-child(7) {
         transition: all 0.5s ease 1s;
         margin: 0;
     }
 
-    li.fade{
+    li.fade {
         opacity: 1;
     }
 
     /* Navigation Bar Icon on Click*/
 
-        .toggle .bars1{
-            transform: rotate(-45deg) translate(-5px, 6px);
-        }
+    .toggle .bars1 {
+        transform: rotate(-45deg) translate(-5px, 6px);
+    }
 
-        .toggle .bars2{
-            transition: all 0s ease;
-            width: 0;
-        }
+    .toggle .bars2 {
+        transition: all 0s ease;
+        width: 0;
+    }
 
-        .toggle .bars3{
-            transform: rotate(45deg) translate(-5px, -6px);
-        }
-
+    .toggle .bars3 {
+        transform: rotate(45deg) translate(-5px, -6px);
+    }
 }
 </style>
--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,47 +1,74 @@
 <header>
-    <nav class="navbar fixed-top">
+    <nav class="navbar w-100 d-flex align-items-center">
         <div class="container-fluid">
 
             <div class="logo_doc">
                 <a href="{{ url('/') }}"><img src="/logo-doc-transp.png" alt="logo"></a>
             </div>
 
-            <button id="offcanvas-btn" class="navbar-toggler navbar-dark" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            @if (Auth::user() == false)
+                <div class="logo_small">
+                    <a href="{{ url('/') }}"><img src="/logo-doc-transp.png" alt="logo"></a>
+                </div>
+            @endif
 
-            <div class="d-flex">
+
+            @auth
+                <button id="offcanvas-btn" class="navbar-toggler navbar-dark" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            @endauth
+
+            <div class="d-flex align-items-center">
                 <ul class="d-flex header-menu m-0">
                     <!-- Authentication Links -->
                     @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
-                    </li>
-                    @endif
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+                            </li>
+                        @endif
                     @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.doctors.index') }}">{{ Auth::user()->name }}</a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.doctors.index') }}">{{ Auth::user()->name }}</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
                         document.getElementById('logout-form').submit();">
-                             <i class="fa-solid fa-right-to-bracket"></i>
-                        </a>
+                                <i class="fa-solid fa-right-to-bracket"></i>
+                            </a>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </li>
                     @endguest
                 </ul>
             </div>
         </div>
     </nav>
 </header>
+
+<style>
+    .logo_small {
+        display: none;
+
+    }
+
+    .logo_small img {
+        width: 16vh;
+        min-width: 200px;
+    }
+
+    @media screen and (max-width: 991px) {
+        .logo_small {
+            display: block;
+        }
+    }
+</style>
